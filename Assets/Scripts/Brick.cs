989bd128cce2abd759f6bf4751123fb61750ce21
Sean Bhupathi <seanbhupathi@gmail.com>
2017-06-07 15:26:54 -0400

    destroying all bricks loads next level (breakableCount in scripts)

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -4,12 +4,21 @@ using System.Collections;
 public class Brick : MonoBehaviour {
 
 	public Sprite[] hitSprites;
+	public static int breakableCount = 0;
 	
 	private int timesHit;
 	private LevelManager levelManager;
+	private bool isBreakable;
 
 	// Use this for initialization
 	void Start () {
+		isBreakable = (this.tag == "Breakable");
+		//Keep track of breakable bricks
+		if(isBreakable){
+			breakableCount++;
+//			print (breakableCount);
+		}
+		
 		timesHit = 0;
 		levelManager = GameObject.FindObjectOfType<LevelManager>();
 	}
@@ -20,7 +29,7 @@ public class Brick : MonoBehaviour {
 	}
 	
 	void OnCollisionEnter2D (Collision2D col) {
-		bool isBreakable = (this.tag == "Breakable");
+		
 		if(isBreakable){
 			HandleHits();
 		}
@@ -32,6 +41,9 @@ public class Brick : MonoBehaviour {
 		int maxHits = hitSprites.Length + 1;
 		
 		if (timesHit >= maxHits){
+			breakableCount--;
+			levelManager.BrickDestroyed ();
+			
 			Destroy(gameObject);
 		}
 		else{
