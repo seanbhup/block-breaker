64c2eba520069566b055d87ce69251108ceafd9e
Sean Bhupathi <seanbhupathi@gmail.com>
2017-06-08 11:01:28 -0400

    added random range for velocity to keep loops of boringness from happening

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -33,8 +33,12 @@ public class Ball : MonoBehaviour {
 	}
 	
 	void OnCollisionEnter2D(Collision2D collision){
+	
+		Vector2 tweak = new Vector2 (Random.Range(0f, 0.2f), Random.Range(0f, 0.2f));
+	
 		if(gameStarted){
 			audio.Play ();
+			rigidbody2D.velocity += tweak;
 		}
 	}
 	
